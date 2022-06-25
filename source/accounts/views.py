1b4bb3a7af9e83699efc7cf652208f0e258df2a2
nooruzbay <nooruzbay@gmail.com>
2022-06-25 18:27:33 +0600

    Added adds into detail profile.view

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import CreateView, DetailView
 from accounts.forms import MyUserCreationForm
+from webapp.models import Advertisement
 
 
 class RegisterView(CreateView):
@@ -30,3 +32,12 @@ class UserDetailView(DetailView):
     template_name = 'user_detailed_view.html'
     model = User
     context_object_name = 'user_object'
+    paginate_by = 10
+    paginate_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        advertisements = Advertisement.objects.filter(author=self.object.pk)
+        if advertisements.filter(Q(status='to_moderate') | Q(status='canceled') | Q(status='published')):
+            kwargs["advertisements"] = advertisements
+            return super().get_context_data(**kwargs)
+
